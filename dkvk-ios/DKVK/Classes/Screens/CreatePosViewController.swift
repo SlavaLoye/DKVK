072ef56334c00fe7ebc496f3c35f47f838334c01
SlavaLoye <electromusiccompany@gmail.com>
2019-01-11 19:17:26 +0300

    add[PostVC]

---
+++ dkvk-ios/DKVK/Classes/Screens/CreatePosViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CreatePosViewController.swift
+//  DKVK
+//
+//  Created by Вячеслав Лойе on 11/01/2019.
+//  Copyright © 2019 Hadevs. All rights reserved.
+//
+
+import UIKit
+
+class CreatePosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
