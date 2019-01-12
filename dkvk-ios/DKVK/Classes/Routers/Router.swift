74c3a3b6a0203655f8f2091e38fd114b4992fed3
SlavaLoye <electromusiccompany@gmail.com>
2019-01-13 01:13:51 +0300

    FixedCreateVC

--- dkvk-ios/DKVK/Classes/Routers/Router.swift
+++ dkvk-ios/DKVK/Classes/Routers/Router.swift
@@ -17,7 +17,6 @@ final class Router {
         let frame = UIScreen.main.bounds
         window = UIWindow(frame: frame)
         window?.makeKeyAndVisible()
-        
         let vc = SecureStorageManager.shared.isLoggedIn() ? startControllerAfterAuth : ViewController()
         
         window?.rootViewController = UINavigationController(rootViewController: vc)
@@ -29,15 +28,17 @@ final class Router {
         let createPostTabBarItem = UITabBarItem(tabBarSystemItem: .more, tag: 0)
         createPostNC.tabBarItem = createPostTabBarItem
         
+        let feedVC = FeedViewController()
+        let feedNC = UINavigationController(rootViewController: feedVC)
+        let feedTabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        feedNC.tabBarItem = feedTabBarItem
+        
         let chatsVC = ChatsViewController()
         let chatsNC = UINavigationController.init(rootViewController: chatsVC)
         let chatsTabbarItem = UITabBarItem(tabBarSystemItem: .contacts, tag: 2)
         chatsNC.tabBarItem = chatsTabbarItem
         
-        let feedVC = FeedViewController()
-        let feedNC = UINavigationController(rootViewController: feedVC)
-        let feedTabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
-        feedNC.tabBarItem = feedTabBarItem
+       
         
         let tabBarVC = UITabBarController()
         tabBarVC.setViewControllers([feedNC, chatsNC, createPostNC], animated: true)
